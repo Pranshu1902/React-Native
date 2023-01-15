92f6c4be5ac814874439e50252dd5c51d7e7fe74
Pranshu Aggarwal <pranshu1902@gmail.com>
2023-01-16 01:05:02 +0530

    drawer navigation added

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  *
+//  * @format
+//  */
 
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import Home from './components/Home';
 import Icon from 'react-native-ionicons';
 import {Blogs} from './components/Blogs';
@@ -18,6 +19,7 @@ import {Counter} from './components/Counter';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
 function StackNavigationContainer() {
   return (
@@ -37,41 +39,63 @@ function StackNavigationContainer() {
   );
 }
 
+function DrawerNavigationContainer() {
+  return (
+    <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Projects" component={Projects} />
+      <Drawer.Screen name="Blogs" component={Blogs} />
+    </Drawer.Navigator>
+  );
+}
+
+function TabNavigatoionContainer() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={StackNavigationContainer}
+        options={{
+          tabBarBadge: 3,
+          tabBarLabel: 'Home',
+          tabBarIcon: () => <Icon name="add" color="#000000" />,
+        }}
+      />
+      <Tab.Screen
+        name="Project"
+        component={Projects}
+        options={{
+          tabBarIcon: () => <Icon name="add" color="#000000" />,
+        }}
+      />
+      <Tab.Screen
+        name="Blog"
+        component={Blogs}
+        options={{
+          tabBarIcon: () => <Icon name="add" color="#000000" />,
+        }}
+      />
+      <Tab.Screen
+        name="Counter"
+        component={Counter}
+        options={{
+          tabBarIcon: () => <Icon name="add" color="#000000" />,
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
 function App(): JSX.Element {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen
-          name="Home"
-          component={StackNavigationContainer}
-          options={{
-            tabBarBadge: 3,
-            tabBarLabel: 'Home',
-            tabBarIcon: () => <Icon name="add" color="#000000" />,
-          }}
-        />
-        <Tab.Screen
-          name="Project"
-          component={Projects}
-          options={{
-            tabBarIcon: () => <Icon name="add" color="#000000" />,
-          }}
-        />
-        <Tab.Screen
-          name="Blog"
-          component={Blogs}
-          options={{
-            tabBarIcon: () => <Icon name="add" color="#000000" />,
-          }}
-        />
-        <Tab.Screen
-          name="Counter"
-          component={Counter}
-          options={{
-            tabBarIcon: () => <Icon name="add" color="#000000" />,
-          }}
-        />
-      </Tab.Navigator>
+      {/* <DrawerNavigationContainer /> */}
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Projects" component={Projects} />
+        <Drawer.Screen name="Blogs" component={Blogs} />
+        <Drawer.Screen name="Tabs" component={StackNavigationContainer} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
